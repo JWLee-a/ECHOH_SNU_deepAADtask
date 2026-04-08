1426962b12ccf6ec3034ad4c07f3b2e92d4a8408
JWLee-a <pome03cogi@gmail.com>
2026-04-08 12:53:48 +0900

    python package 정보 (requirements.txt), bat 파일 추가

---
+++ RUN/run_ASA.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d C:\Users\KIST\PycharmProjects\SNUexp\ASA
+C:\Users\KIST\PycharmProjects\SNUexp\.venv\Scripts\python.exe ASA_Korean_KIST_v2.0.py
+pause
---
+++ RUN/run_validation.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d C:\Users\KIST\PycharmProjects\SNUexp\ASA
+C:\Users\KIST\PycharmProjects\SNUexp\.venv\Scripts\python.exe Validation_v1.0.py
+pause
